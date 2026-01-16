5d377767a415856e02d3bf54ce1f4b1efe42dc27
Samuel <sae.nieto@gmail.com>
2026-01-16 16:36:32 +0100

    fix  build 3

--- src/features/team-builder/components/PokemonGrid.jsx
+++ src/features/team-builder/components/PokemonGrid.jsx
@@ -21,7 +21,7 @@ export default function PokemonGrid({ searchTerm = "", selectedType = "" }) {
 
   const totalPages = Math.ceil(totalResults / ITEMS_PER_PAGE);
 
-  if (isLoading && page === 0) {
+  if (isLoading) {
     return (
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
         {[...Array(ITEMS_PER_PAGE)].map((_, index) => (
